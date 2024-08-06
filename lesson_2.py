20e672df0b7506f3abefd8d46ab30b77a29665a1
Felix Omondi <felixomosh7@gmail.com>
2024-08-06 12:51:43 +0300

    initializa an object method

--- lesson_2.py
+++ lesson_2.py
@@ -4,6 +4,13 @@ class Person:
         self.name = name
         self.age = age
 
+    def __str__(self) -> str:
+        return f"{self.name} is {self.age} years old."
+    
+    def my_func(self):
+        return f"Hello, my name is {self.name}"
+
 person1 = Person(name="Jon", age=40)
 print(person1.age)
 print(person1.name)
+person1.my_func()
