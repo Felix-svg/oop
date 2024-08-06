a3c9ff319c88dc01e836902f0d352b1b2cbe246c
Felix Omondi <felixomosh7@gmail.com>
2024-08-06 12:39:32 +0300

    First commit

---
+++ lesson_1.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Employee(Person):
+    def __init__(self, first_name, last_name, age, salary, employment_year):
+        super().__init__(first_name, last_name)
+        self.age = age
+        self.salary = salary
+        self.employment_year = employment_year
+
+    def employee(self):
+        return f"{self.first_name} {self.last_name} joined this organization in the year {self.employment_year}, and earns a salary of Ksh.{self.salary}."
+
+
+felix = Employee(
+    first_name="Felix", last_name="Omondi", age=25, salary=40000, employment_year=2024
+)
+print(felix.employee())
